e113c66c2b23315a9973940a6bd7070638d63301
Naufaleriansyah <naufaleriansyah07@gmail.com>
2019-10-31 11:03:00 +0700

    TUGASDASARMANDIRI

---
+++ Bab II/deklarasi.cppnfl.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int jumlah uang ;
+    jumlah uang =1000;
+    cout << "jumlah uang semula :" << jumlah uang << endl;
+
+    // variabel diubah
+
+    jumlah uang =1200
+    cout << "jumlah uang sekarang: " << jumlah uang << endl;
+
+    return 0;
+    }
